8515ddde6ea0e8c421f80e9d5a3304c9605b7bba
Geoff Lamrock <geoff.lamrock@gmail.com>
2023-08-24 08:50:19 +1000

    feat: Adds support for authenticating with access token (#21)

--- src/input-parameters.ts
+++ src/input-parameters.ts
@@ -4,6 +4,7 @@ import { PromptedVariableValues } from '@octopusdeploy/api-client'
 const EnvironmentVariables = {
   URL: 'OCTOPUS_URL',
   ApiKey: 'OCTOPUS_API_KEY',
+  AccessToken: 'OCTOPUS_ACCESS_TOKEN',
   Space: 'OCTOPUS_SPACE'
 } as const
 
@@ -11,7 +12,9 @@ export interface InputParameters {
   // Optional: A server is required, but you should use the OCTOPUS_URL env
   server: string
   // Optional: An API key is required, but you should use the OCTOPUS_API_KEY environment variable instead of this.
-  apiKey: string
+  apiKey?: string
+  // Optional: Access token can only be obtained from the OCTOPUS_ACCESS_TOKEN environment variable.
+  accessToken?: string
   // Optional: You should prefer the OCTOPUS_SPACE environment variable
   space: string
   // Required
@@ -39,7 +42,8 @@ export function getInputParameters(): InputParameters {
 
   const parameters: InputParameters = {
     server: getInput('server') || process.env[EnvironmentVariables.URL] || '',
-    apiKey: getInput('api_key') || process.env[EnvironmentVariables.ApiKey] || '',
+    apiKey: getInput('api_key') || process.env[EnvironmentVariables.ApiKey],
+    accessToken: process.env[EnvironmentVariables.AccessToken],
     space: getInput('space') || process.env[EnvironmentVariables.Space] || '',
     project: getInput('project', { required: true }),
     releaseNumber: getInput('release_number', { required: true }),
@@ -53,17 +57,19 @@ export function getInputParameters(): InputParameters {
   const errors: string[] = []
   if (!parameters.server) {
     errors.push(
-      "The Octopus instance URL is required, please specify explictly through the 'server' input or set the OCTOPUS_URL environment variable."
+      "The Octopus instance URL is required, please specify explicitly through the 'server' input or set the OCTOPUS_URL environment variable."
     )
   }
-  if (!parameters.apiKey) {
+
+  if (!parameters.apiKey && !parameters.accessToken) {
     errors.push(
-      "The Octopus API Key is required, please specify explictly through the 'api_key' input or set the OCTOPUS_API_KEY environment variable."
+      "The Octopus API Key is required, please specify explicitly through the 'api_key' input or set the OCTOPUS_API_KEY environment variable."
     )
   }
+
   if (!parameters.space) {
     errors.push(
-      "The Octopus space name is required, please specify explictly through the 'space' input or set the OCTOPUS_SPACE environment variable."
+      "The Octopus space name is required, please specify explicitly through the 'space' input or set the OCTOPUS_SPACE environment variable."
     )
   }
 
